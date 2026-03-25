b753f78fb72505a057b6811efe93e10ddc273858
hottube2 <b_levesque@hotmail.com>
2026-03-25 08:34:00 -0400

    Update Stripe API version to 2023-10-16

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -9,7 +9,7 @@ export function getStripeServer() {
 
   if (!stripe) {
     stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2024-12-18.acacia"
+      apiVersion: "2023-10-16"
     });
   }
 
